2927cb9c8d97ad03eb0c436258f0b636e6604c6f
Pavan Kumar Tulasi <thulasipavankumar@gmail.com>
2017-11-24 14:30:24 +0530

    added submodule clone

--- src/main/java/com/tulasi/immutablelist/ImmutableList.java
+++ src/main/java/com/tulasi/immutablelist/ImmutableList.java
@@ -5,9 +5,14 @@
  */
 package com.tulasi.immutablelist;
 
+import com.rits.cloning.Cloner;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  *
@@ -32,38 +37,19 @@ public final class ImmutableList implements Cloneable{
     }
     
     private  Object returnACopyOfObject(final Object obj) throws Exception {
-        try{
-            Object clone = obj.getClass().newInstance();
-            for (Field field : obj.getClass().getDeclaredFields()) {
-                field.setAccessible(true);
-                if(field.get(obj) == null || Modifier.isFinal(field.getModifiers())){
-                    continue;
-                }
-                if(field.getType().isPrimitive() || field.getType().equals(String.class)
-                        || field.getType().getSuperclass().equals(Number.class)
-                        || field.getType().equals(Boolean.class)){
-                    field.set(clone, field.get(obj));
-                }else{
-                    Object childObj = field.get(obj);
-                    if(childObj == obj){
-                        field.set(clone, clone);
-                    }else{
-                        field.set(clone, returnACopyOfObject(field.get(obj)));
-                    }
-                }
-            }
-            return clone;
-        }catch(Exception e){
-            e.printStackTrace();
-            return null;
-        }
+        Cloner cloner=new Cloner();
+        Object newClone = cloner.deepClone(obj);
+        return newClone;
+
     }
+    
+    
 
     private ArrayList returnCloneForTheList(int start, int end) throws Exception {
         if (start >= end) {
             throw new Exception("start should not be greater than end");
         }
-        ArrayList newList = getAListWithinGivenBounds(start, end, new ArrayList());
+        ArrayList newList = getAListWithinGivenBounds(start, end, getANewInstanceOfArrayList());
         return newList;
     }
 
@@ -74,6 +60,9 @@ public final class ImmutableList implements Cloneable{
         }
 
         return newList;
+    }
+    private ArrayList getANewInstanceOfArrayList(){
+        return new ArrayList();
     }
     private ArrayList getReverselist(int start, int end, ArrayList newList) throws Exception {
         
@@ -106,25 +95,25 @@ public final class ImmutableList implements Cloneable{
 
     public ArrayList tail() throws Exception {
         int start = 1, end = list.size();
-        ArrayList newList = getAListWithinGivenBounds(start, end, new ArrayList());
+        ArrayList newList = getAListWithinGivenBounds(start, end, getANewInstanceOfArrayList());
         return newList;
     }
     public ArrayList reverse() throws Exception{
         if(list.size()==0)
             return null;
-        ArrayList reverseList = getReverselist(0, list.size(), new ArrayList());
+        ArrayList reverseList = getReverselist(0, list.size(), getANewInstanceOfArrayList());
         return reverseList;
     }
     public ArrayList drop(int index) throws Exception{
         if(index>list.size())
             return null;
-        ArrayList newlist = getAListWithinGivenBounds(index, list.size(), new ArrayList());
+        ArrayList newlist = getAListWithinGivenBounds(index, list.size(), getANewInstanceOfArrayList());
         return newlist;   
     }
     public ArrayList cons(Object obj) throws Exception{
-         ArrayList newlist  = new ArrayList();
+         ArrayList newlist  = getANewInstanceOfArrayList();
          list.add(obj);
-       ArrayList tempList = getAListWithinGivenBounds(0, list.size(), new ArrayList());
+       ArrayList tempList = getAListWithinGivenBounds(0, list.size(), getANewInstanceOfArrayList());
        for(Object tempObj:tempList)
            list.add(tempObj);
         return newlist;   
